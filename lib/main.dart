ac6431c4fee7d8d886b562a285ef7d066f8e4bb8
unknown <amoshal1997@gmail.com>
2025-06-27 21:33:28 +0300

    added new styles

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 import 'providers/auth_provider.dart';
@@ -29,8 +30,164 @@ class MyApp extends StatelessWidget {
         builder: (context, themeProvider, _) {
           return MaterialApp(
             title: 'Word Learning App',
-            theme: ThemeData.light(useMaterial3: true),
-            darkTheme: ThemeData.dark(useMaterial3: true),
+            theme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.deepPurple,
+                primary: Colors.deepPurple[700],
+                secondary: Colors.amber[600],
+                surface: Colors.white,
+                onSurface: Colors.black87,
+              ),
+              scaffoldBackgroundColor: Colors.grey[100],
+              appBarTheme: AppBarTheme(
+                backgroundColor: Colors.deepPurple[700],
+                foregroundColor: Colors.white,
+                elevation: 0,
+                centerTitle: true,
+                titleTextStyle: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple[700],
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  textStyle: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  elevation: 2,
+                ),
+              ),
+              textButtonTheme: TextButtonThemeData(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.deepPurple[700],
+                  textStyle: GoogleFonts.poppins(fontSize: 16),
+                ),
+              ),
+              textTheme: GoogleFonts.poppinsTextTheme(
+                const TextTheme(
+                  displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  headlineSmall: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                  bodyLarge: TextStyle(fontSize: 16, color: Colors.black87),
+                  bodyMedium: TextStyle(fontSize: 14, color: Colors.black54),
+                ),
+              ),
+              cardTheme: CardThemeData(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              ),
+              inputDecorationTheme: InputDecorationTheme(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(color: Colors.deepPurple[200]!),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(color: Colors.deepPurple[700]!, width: 2),
+                ),
+                labelStyle: TextStyle(color: Colors.deepPurple[700]),
+                filled: true,
+                fillColor: Colors.deepPurple[50],
+                prefixIconColor: Colors.deepPurple[700],
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              ),
+              pageTransitionsTheme: const PageTransitionsTheme(
+                builders: {
+                  TargetPlatform.android: ZoomPageTransitionsBuilder(),
+                  TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+                },
+              ),
+            ),
+            darkTheme: ThemeData(
+              useMaterial3: true,
+              colorScheme: ColorScheme.fromSeed(
+                seedColor: Colors.deepPurple,
+                primary: Colors.deepPurple[300],
+                secondary: Colors.amber[400],
+                surface: Colors.grey[850],
+                onSurface: Colors.white70,
+              ),
+              scaffoldBackgroundColor: Colors.grey[900],
+              appBarTheme: AppBarTheme(
+                backgroundColor: Colors.deepPurple[300],
+                foregroundColor: Colors.black,
+                elevation: 0,
+                centerTitle: true,
+                titleTextStyle: GoogleFonts.poppins(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.black,
+                ),
+              ),
+              elevatedButtonTheme: ElevatedButtonThemeData(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple[300],
+                  foregroundColor: Colors.black,
+                  padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  textStyle: GoogleFonts.poppins(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w600,
+                  ),
+                  elevation: 2,
+                ),
+              ),
+              textButtonTheme: TextButtonThemeData(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.deepPurple[300],
+                  textStyle: GoogleFonts.poppins(fontSize: 16),
+                ),
+              ),
+              textTheme: GoogleFonts.poppinsTextTheme(
+                const TextTheme(
+                  displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                  headlineSmall: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
+                  bodyLarge: TextStyle(fontSize: 16, color: Colors.white70),
+                  bodyMedium: TextStyle(fontSize: 14, color: Colors.white60),
+                ),
+              ),
+              cardTheme: CardThemeData(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              ),
+              inputDecorationTheme: InputDecorationTheme(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(color: Colors.deepPurple[800]!),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(12),
+                  borderSide: BorderSide(color: Colors.deepPurple[300]!, width: 2),
+                ),
+                labelStyle: TextStyle(color: Colors.deepPurple[300]),
+                filled: true,
+                fillColor: Colors.grey[800],
+                prefixIconColor: Colors.deepPurple[300],
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              ),
+              pageTransitionsTheme: const PageTransitionsTheme(
+                builders: {
+                  TargetPlatform.android: ZoomPageTransitionsBuilder(),
+                  TargetPlatform.iOS: CupertinoPageTransitionsBuilder(),
+                },
+              ),
+            ),
             themeMode: themeProvider.isDarkMode ? ThemeMode.dark : ThemeMode.light,
             home: Consumer<AuthProvider>(
               builder: (context, authProvider, _) {
